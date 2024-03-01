aa372c65ba79b749610d46021cc3270657007bf4
Arnav G <73305591+arnavg115@users.noreply.github.com>
2024-03-01 00:45:23 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
    name='agrad',
-   version='1.0',
+   version='1.0.1',
    description='A homecooked autograd library built w/ numpy.',
    author='arnavg115',
-   packages=['agrad'],  #same as name
+   packages=['agrad','agrad.nn'],  #same as name
    install_requires=['numpy'], #external packages as dependencies
 )
